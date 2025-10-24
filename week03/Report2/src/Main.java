4bcd78ab352f319331105b6e656c8f3a0d53e57c
2501014 <kypneasrzyns@navrr.com>
2025-10-24 20:35:44 +0900

    3주차 과제

---
+++ week03/Report2/src/Main.java
@@ -0,0 +1,13 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("***************************");
+        System.out.println("학교:경복대학교");
+        System.out.println("이름:홍길동");
+        System.out.println("나이:20");
+        System.out.println("성별:남(여)");
+        System.out.println("체중:65,4kg");
+        System.out.println("***************************");
+    }
+}
